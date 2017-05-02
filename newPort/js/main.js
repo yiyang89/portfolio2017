8fa6458279d3e7e3636ce6fbefcc6171fa2add02
yiyang89 <yiyangkok@gmail.com>
2017-05-02 15:58:07 -0700

    swipe navigation and arrow key navigation complete

--- newPort/js/main.js
+++ newPort/js/main.js
@@ -14,12 +14,27 @@ $(document).ready(function(){
     slidesToScroll: 1,
     dots: true,
     infinite: false,
-    cssEase: 'linear',
+    cssEase: 'ease',
     swipeToSlide: true,
     swipe: true,
     vertical: true,
     appendDots: $('.sliderdots'),
     arrows: false,
     responsive: true
+  })
+  $.browserSwipe({
+    up: function(){
+      $('.vertical-slider').slick('slickPrev');
+    },
+    down: function(){
+      $('.vertical-slider').slick('slickNext');
+    }
+  });
+  $('body').keydown(function(event) {
+    if (event.keyCode == 38 || event.keyCode == 37) {
+      $('.vertical-slider').slick('slickPrev');
+    } else if (event.keyCode == 40 || event.keyCode == 39) {
+      $('.vertical-slider').slick('slickNext');
+    }
   })
 });
