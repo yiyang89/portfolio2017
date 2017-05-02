7991d4cd2712b4b35bcd1241897327ab3bb9d22f
yiyang89 <yiyangkok@gmail.com>
2017-05-02 15:26:00 -0700

    vertical sliding page layout in place

--- newPort/js/main.js
+++ newPort/js/main.js
@@ -6,6 +6,20 @@ $(document).ready(function(){
     infinite: true,
     cssEase: 'linear',
     swipeToSlide: true,
-    swipe: true
+    swipe: true,
+    responsive: true
   });
+  $('.vertical-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: false,
+    cssEase: 'linear',
+    swipeToSlide: true,
+    swipe: true,
+    vertical: true,
+    appendDots: $('.sliderdots'),
+    arrows: false,
+    responsive: true
+  })
 });
