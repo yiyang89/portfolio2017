a1dabe135592bb6c64f8087cdbcd60dcee6fdcfc
yiyang89 <yiyangkok@gmail.com>
2017-05-02 14:00:52 -0700

    portfolio items in horizontal slider

---
+++ newPort/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.single-item').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    cssEase: 'linear',
+    swipeToSlide: true,
+    swipe: true
+  });
+});
