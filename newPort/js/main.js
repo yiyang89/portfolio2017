1e4bd0150701f436c0ecb9b9ff4fe4a9619d1289
yiyang89 <yiyangkok@gmail.com>
2017-05-04 03:21:26 -0700

    some preliminary mobile layout

--- newPort/js/main.js
+++ newPort/js/main.js
@@ -17,21 +17,22 @@ $(document).ready(function(){
     dots: true,
     infinite: false,
     cssEase: 'ease',
-    swipe: true,
+    // swipe: true,
+    swipe: false,
     vertical: true,
     appendDots: $('.sliderdots'),
     arrows: false,
     responsive: true,
     accessibility: false
   });
-  $.browserSwipe({
-    up: function(){
-      $('.vertical-slider').slick('slickPrev');
-    },
-    down: function(){
-      $('.vertical-slider').slick('slickNext');
-    },
-  });
+  // $.browserSwipe({
+  //   up: function(){
+  //     $('.vertical-slider').slick('slickPrev');
+  //   },
+  //   down: function(){
+  //     $('.vertical-slider').slick('slickNext');
+  //   },
+  // });
   $('body').keydown(function(event) {
     if (event.keyCode == 38) {
       $('.vertical-slider').slick('slickPrev');
@@ -43,20 +44,20 @@ $(document).ready(function(){
       $('.single-item').slick('slickNext');
     }
   });
-  $('body').on('swipeup',function(event) {
-    $('.vertical-slider').slick('slickPrev');
-  });
-  $('body').on('swipedown',function(event) {
-    $('.vertical-slider').slick('slickNext');
-  });
-  $('body').on('swipeleft', function(event){
-    console.log("swipeleft");
-    $('.single-item').slick('slickPrev');
-  });
-  $('body').on('swiperight', function(event){
-    console.log("swiperight");
-    $('.single-item').slick('slickNext');
-  });
+  // $('body').on('swipeup',function(event) {
+  //   $('.vertical-slider').slick('slickPrev');
+  // });
+  // $('body').on('swipedown',function(event) {
+  //   $('.vertical-slider').slick('slickNext');
+  // });
+  // $('body').on('swipeleft', function(event){
+  //   console.log("swipeleft");
+  //   $('.single-item').slick('slickPrev');
+  // });
+  // $('body').on('swiperight', function(event){
+  //   console.log("swiperight");
+  //   $('.single-item').slick('slickNext');
+  // });
   $('.fa-arrow-down').click(function() {
     $('.vertical-slider').slick('slickNext');
   });
