729debf2d978f08a8cfaab3440f99c4d65aab358
yiyang89 <yiyangkok@gmail.com>
2017-05-04 00:34:54 -0700

    added email to third slide

--- newPort/js/main.js
+++ newPort/js/main.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
     dots: true,
     infinite: false,
     cssEase: 'ease',
-    swipeToSlide: true,
     swipe: true,
     vertical: true,
     appendDots: $('.sliderdots'),
@@ -48,10 +47,52 @@ $(document).ready(function(){
       $('.single-item').slick('slickNext');
     }
   });
+  $('body').on('swipeup',function(event) {
+    $('.vertical-slider').slick('slickPrev');
+  });
+  $('body').on('swipedown',function(event) {
+    $('.vertical-slider').slick('slickNext');
+  })
   $('.fa-arrow-down').click(function() {
     $('.vertical-slider').slick('slickNext');
   });
   $('.fa-arrow-up').click(function() {
     $('.vertical-slider').slick('slickPrev');
   })
+
+  // var touchstartX = 0;
+  // var touchstartY = 0;
+  // var touchendX = 0;
+  // var touchendY = 0;
+//
+  // document.body.addEventListener('touchstart', function(event) {
+  //     // touchstartX = event.screenX;
+  //     // touchstartY = event.screenY;
+  //     touchstartX = event.changedTouches[0].screenX;
+  //     touchstartY = event.changedTouches[0].screenY;
+  // }, false);
+  //
+  // document.body.addEventListener('touchend', function(event) {
+  //     // touchendX = event.screenX;
+  //     // touchendY = event.screenY;
+  //     touchendX = event.changedTouches[0].screenX;
+  //     touchendY = event.changedTouches[0].screenY;
+  //     handleGesture();
+  // }, false);
+  //
+  // function handleGesture() {
+  //     // alert('Swipe');
+  //     // console.log('swipe');
+  //     var swiped = 'swiped:';
+  //     // console.log("START Y:" + touchstartY);
+  //     // console.log("END Y:"+touchendY);
+  //     if (touchendY < touchstartY) {
+  //       // console.log("swipe up");
+  //       $('.vertical-slider').slick('slickPrev');
+  //     }
+  //     if (touchendY > touchstartY) {
+  //       // console.log("swipe down");
+  //       $('.vertical-slider').slick('slickNext');
+  //     }
+  // }
 });
